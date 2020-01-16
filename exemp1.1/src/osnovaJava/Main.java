cc5303e9d4492c778984765817b2ee6f78805cd9
sveta <myemail.yandex.ru>
2020-01-16 19:16:35 +0300

    задание 1.2

---
+++ exemp1.1/src/osnovaJava/Main.java
@@ -0,0 +1,16 @@
+package osnovaJava;
+
+public class Main {
+
+    public static void main(String[] args) {
+	byte a = 2;
+	short b = 120;
+	int d = 109980;
+	long c = 898857657;
+	float t = 1.8f;
+	double s = 276.98;
+	char f = 'y';
+        System.out.println(a);
+		System.out.println(f);
+    }
+}
